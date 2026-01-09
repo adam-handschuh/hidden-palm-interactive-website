be9409891d89e8760f026ed7daae08d52f186123
Adam <adam.o.handschuh@gmail.com>
2026-01-09 10:37:44 +0000

    First commit

---
+++ app.js
@@ -0,0 +1,51 @@
+const buttons = document.querySelectorAll(".header_button");
+const pages = document.querySelectorAll(".info_page");
+
+buttons.forEach(button => {
+
+  button.addEventListener("click", () => {
+    // Hide all pages
+    pages.forEach(page => page.classList.remove("show"));
+
+    // Reset button styles
+    buttons.forEach(btn => btn.classList.remove("active"));
+
+    // Show selected page
+    const targetPage = document.getElementById(button.dataset.target);
+    targetPage.classList.add("show");
+
+    // Highlight active button
+    button.classList.add("active");
+  });
+});
+
+
+const selector_buttons = document.querySelectorAll(".select_button");
+const selector_underlay = document.getElementById("selector_underlay");
+const screenshot = document.getElementById("screenshot");
+const fade_box = document.getElementById("fade_box");
+
+selector_buttons.forEach(sButton =>{
+  sButton.addEventListener("click", () =>{
+    for(let i = 1; i <= 5; i ++){
+      selector_underlay.classList.remove("_" + i);
+    }
+    selector_underlay.classList.add(sButton.id);
+
+    reset_animation(fade_box);
+    fade_box.style.animation = "fade-in .5s linear 1";
+    if(sButton.id != "_1"){
+      screenshot.style.display = "block";
+      screenshot.src = "images/screenshot" + sButton.id + ".jpg";
+    }else{
+      screenshot.style.display = "none";
+    }
+    fade_box.style.animation = "fade-out .5s linear 1";
+  });
+});
+
+function reset_animation(element) {
+  element.style.animation = 'none';
+  element.offsetHeight;
+  element.style.animation = null; 
+}
